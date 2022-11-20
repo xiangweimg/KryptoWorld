0c924cd2049b2f9967037429ed9c6b19661dd8b9
Xiangwei Liu <xiangweimg@gmail.com>
2022-11-19 21:34:20 -0500

    11.19

--- src/javascript/graph.js
+++ src/javascript/graph.js
@@ -7,31 +7,44 @@ const coin_api = (coin)=>{
         axios.get(`https://api.coingecko.com/api/v3/coins/${coin}/market_chart?vs_currency=usd&days=30&interval=daily`)
     )
 }
+const coin_api2 = (coin)=>{
+    return Promise.resolve(
+        axios.get(`https://api.coingecko.com/api/v3/coins/${coin}/market_chart?vs_currency=usd&days=7&interval=daily`)
+    )
+}
+
 
 document.addEventListener("DOMContentLoaded", e => {
     const ctx = document.getElementById('myChart').getContext('2d');
-
+    let status = true
+    let text = "bitcoin"
     let days = [];
     let prices = [];
     let myChart;
     function getDataAndGenerateChartAftewards(info, createChartCallback, chartOptions, isUpdate = true) {
-        coin_api(info)
-        .then(({ data }) => {
-            if (isUpdate) {
-                days = [];
-                prices = [];
-            }
-            data.prices.forEach(price => {
-                    let date = new Date(price[0])
-                    days.push(date.toLocaleDateString("en-US"));
-                    prices.push(price[1]);
-                }
-            );
-        }).then(() => {
-            createChartCallback(info, chartOptions);
-        }).catch((error) => {
-            console.log("invalid days");
-        });
+        let get_api = coin_api(info)
+        if(status === true){
+            get_api = coin_api(info)
+        }
+        else{
+            get_api = coin_api2(info)
+        }           
+                    get_api.then(({ data }) => {
+                    if (isUpdate) {
+                        days = [];
+                        prices = [];
+                    }
+                    data.prices.forEach(price => {
+                            let date = new Date(price[0])
+                            days.push(date.toLocaleDateString("en-US"));
+                            prices.push(price[1]);
+                        }
+                    );
+                }).then(() => {
+                    createChartCallback(info, chartOptions);
+                }).catch((error) => {
+                    alert('Error: Invalid Search!! Please Enter the Fullname of Coins')
+                })  
     }
 
     function createChartCallback(info, { backgroundColor, borderColor }) {
@@ -101,9 +114,12 @@ document.addEventListener("DOMContentLoaded", e => {
             },
         };
         myChart = new Chart(ctx, data);
+        text = data.data.datasets[0].label
     }
 
     function updateChartCallback(info, { backgroundColor, borderColor }) { 
+        text = info
+        // console.log(text);
         const data = {
             labels: days,
             datasets: [{
@@ -116,6 +132,8 @@ document.addEventListener("DOMContentLoaded", e => {
         };
         myChart.data = data;
         myChart.update();
+        text = myChart.data.datasets[0].label
+        // console.log(text);
     }
     // window.updateChart = updateChartCallback;
 
@@ -124,19 +142,46 @@ document.addEventListener("DOMContentLoaded", e => {
     getDataAndGenerateChartAftewards(coinName, createChartCallback, {backgroundColor: 'white', borderColor: 'blue'}, false);
 
     const coinBtns = document.querySelectorAll('.logo');
+
     coinBtns.forEach(btn => {
         btn.addEventListener("click", e => {
             let chartOptions = e.currentTarget.dataset;
             coinName = e.currentTarget.id;
             getDataAndGenerateChartAftewards(coinName, updateChartCallback, chartOptions);
-        });
+        })
     })
     const search_coin = document.getElementById('search-button');
     const input = document.getElementById('search-box');
-
+    const graphDate = document.getElementById('graph-days')
     search_coin.addEventListener("click", e=> {
-        console.log(input.value)
-        getDataAndGenerateChartAftewards(input.value, updateChartCallback, {backgroundColor: 'white', borderColor: 'black'});
+        getDataAndGenerateChartAftewards(input.value, updateChartCallback, {backgroundColor: 'white', borderColor: 'green'});
+        graphDate.textContent = " 30 ";
     });
-});
+    
+    // Execute a function when the user presses a key on the keyboard
+        input.addEventListener("keypress", function(event) {
+      // If the user presses the "Enter" key on the keyboard
+      if (event.key === "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        search_coin.click();
+      }
+    });
+    const monthly = document.getElementById('monthly-button')
+    const weekly = document.getElementById('weekly-button')
 
+    weekly.addEventListener("click", e => {
+        status = false
+        getDataAndGenerateChartAftewards(text, updateChartCallback, {backgroundColor: 'white', borderColor: 'green'});
+        status = true
+        graphDate.textContent = '7';
+    })
+    monthly.addEventListener("click", e => {
+        status = true
+        getDataAndGenerateChartAftewards(text, updateChartCallback, {backgroundColor: 'white', borderColor: 'green'});
+        graphDate.textContent = "30 ";
+    })
+
+    
+});
